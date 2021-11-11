e89201bd68d201e9a64cb08d93d8d300c4c8ccfc
Pratim Chaudhuri <pratim@chaudhuri.me>
2021-11-11 12:56:00 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,16 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+
+  bucket_prefix	= "${var.prefix}"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+tags = {
+    owner = "${var.prefix}-gaurav"
+  }
+
+}
